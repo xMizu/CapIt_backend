c679ba63085d0f6da2046390d1c3d1d17f5b7453
Anthony <anthonywan00@gmail.com>
2019-10-02 13:06:17 -0400

    income to render user

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -5,7 +5,7 @@ class IncomesController < ApplicationController
         user = User.find(user_id)
         income = Income.new(title: params[:name], amount: params[:amount].to_i, user_id: user_id)
         if income.save
-            render json: user.incomes
+            render json: user
         else
             render json: {status: "error", code: 5000, message: income.errors.full_messages[0]}
         end
