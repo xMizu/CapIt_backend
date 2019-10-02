7349e30217d1603bd33831bb37abc15bb69967e4
Anthony <anthonywan00@gmail.com>
2019-10-02 17:19:54 -0400

    added validator for negative amounts

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,15 @@ class Expense < ApplicationRecord
   belongs_to :user
   validates :amount, presence: true
   validates :name, presence: true
+  validate :amount_not_negative
+
+
+  def amount_not_negative
+    if amount <= 0
+      errors.add(:amount, "has to be greater than 0")
+    end
+  end
+
 
 
 end
