1ab90aefab835fb64a98b7d55df5c63317f155d9
Anthony <anthonywan00@gmail.com>
2019-10-03 12:52:33 -0400

    added additional renders on expense creation

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,10 @@
 class ExpensesController < ApplicationController
     def create
+        token = request.headers['Authorization']
+        user_id = JWT.decode(token, 'secret')[0]["userId"]
+        user = User.find(user_id)
         expense = Expense.new(user_id: params[:user_id],category_id: params[:category_id], name: params[:name], amount: params[:amount].to_i, goal: false)
         if expense.save
-            user = User.find(params[:user_id]).expenses.where(goal: false).order(created_at: :desc)
             render json: user
         else
             render json: {status: "error", message: expense.errors.full_messages}
